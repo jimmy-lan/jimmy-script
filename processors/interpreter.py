9f0603709b44ee1e23e5421b89bb2e11dc84ddf0
Jimmy <jimmylanbcn@gmail.com>
2021-02-20 17:39:14 -0500

    Add structure for Interpreter class

--- processors/interpreter.py
+++ processors/interpreter.py
@@ -1,6 +1,23 @@
+from nodes.bin_op_node import BinOpNode
 from nodes.node import Node
+from nodes.number_node import NumberNode
+from nodes.unary_op_node import UnaryOpNode
 
 
 class Interpreter:
-    def evaluate(self, node: Node):
-        pass
+    def traverse(self, node: Node):
+        method_name = f"traverse_{type(node).__name__}"
+        method = getattr(self, method_name, self.traverse_fallback)
+        return method(node)
+
+    def traverse_NumberNode(self, node: NumberNode):
+        print("number node")
+
+    def traverse_BinOpNode(self, node: BinOpNode):
+        print("bin op node")
+
+    def traverse_UnaryOpNode(self, node: UnaryOpNode):
+        print("unary op node")
+
+    def traverse_fallback(self, node):
+        raise Exception(f"No method for {type(node).__name__} defined.")
