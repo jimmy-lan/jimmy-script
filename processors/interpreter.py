c6acbc2017149f2cd8e951252638adc0e0c843ed
Jimmy <jimmylanbcn@gmail.com>
2021-02-20 21:06:38 -0500

    Apply InterpreterPromise

--- processors/interpreter.py
+++ processors/interpreter.py
@@ -3,6 +3,7 @@ from nodes.bin_op_node import BinOpNode
 from nodes.node import Node
 from nodes.number_node import NumberNode
 from nodes.unary_op_node import UnaryOpNode
+from processors.promises import InterpreterPromise
 from values.number import Number
 
 
@@ -17,37 +18,50 @@ class Interpreter:
         # Execute method
         return traverse_method(node)
 
-    def traverse_NumberNode(self, node: NumberNode) -> Number:
+    def traverse_NumberNode(self, node: NumberNode) -> InterpreterPromise:
         token = node.token
-        return Number(token.value, token.interval)
+        return InterpreterPromise().resolve(Number(token.value, token.interval))
 
-    def traverse_BinOpNode(self, node: BinOpNode) -> Number:
-        left: Number = self.traverse(node.left)
-        right: Number = self.traverse(node.right)
+    def traverse_BinOpNode(self, node: BinOpNode) -> InterpreterPromise:
+        promise = InterpreterPromise()
+        left: Number = promise.register(self.traverse(node.left))
+        if promise.error:
+            return promise
+        right: Number = promise.register(self.traverse(node.right))
+        if promise.error:
+            return promise
 
         op_type = node.token.type
         if op_type == TOKEN_PLUS:
-            result = left.add(right)
+            result, error = left.add(right)
         elif op_type == TOKEN_MINUS:
-            result = left.subtract(right)
+            result, error = left.subtract(right)
         elif op_type == TOKEN_MULTIPLY:
-            result = left.multiply(right)
+            result, error = left.multiply(right)
         elif op_type == TOKEN_DIVISION:
-            result = left.divide(right)
+            result, error = left.divide(right)
         else:
             raise Exception(f"Unknown token type '{op_type}' not handled by parser.")
 
-        result.interval = node.interval
-        return result
+        if error is not None:
+            return promise.reject(error)
 
-    def traverse_UnaryOpNode(self, node: UnaryOpNode) -> Number:
-        result = self.traverse(node.child)
+        result.interval = node.interval
+        return promise.resolve(result)
+
+    def traverse_UnaryOpNode(self, node: UnaryOpNode) -> InterpreterPromise:
+        promise = InterpreterPromise()
+        result = promise.register(self.traverse(node.child))
+        if promise.error:
+            return promise
 
         if node.token.type == TOKEN_MINUS:
-            result = Number(0).subtract(result)
+            result, error = Number(0).subtract(result)
+            if error is not None:
+                return promise.reject(error)
 
         result.interval = node.interval
-        return result
+        return promise.resolve(result)
 
     def traverse_fallback(self, node) -> None:
         raise Exception(f"No method for {type(node).__name__} defined.")
